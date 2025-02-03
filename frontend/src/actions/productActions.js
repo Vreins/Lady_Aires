dfe642f355ee62a095aed3eef09efdcc24d99f8e
AdahAugustine <72761880+Vreins@users.noreply.github.com>
2025-02-03 12:48:04 +0100

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -31,7 +31,7 @@ export const listProducts = ({ seller = '' , name = '', category='',   order = '
     type: PRODUCT_LIST_REQUEST,
   });
   try {
-    const { data } = await Axios.get(`https://lady-aires-wc74.onrender.com/api/products?seller=${seller}&name=${name}&category=${category}&min=${min}&max=${max}&rating=${rating}&order=${order}`);
+    const { data } = await Axios.get(`/api/products?seller=${seller}&name=${name}&category=${category}&min=${min}&max=${max}&rating=${rating}&order=${order}`);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
