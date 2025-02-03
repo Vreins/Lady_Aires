6147425aa87f126f454a300b3a03d238e7bcabd7
AdahAugustine <72761880+Vreins@users.noreply.github.com>
2025-02-03 12:09:23 +0100

    Update build

--- backend/server.js
+++ backend/server.js
@@ -48,9 +48,9 @@ app.get('/', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '../frontend/src')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/src/index.js'))
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
   );
 
 
